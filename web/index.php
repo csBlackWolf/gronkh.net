320c788a0c5a1d3da8772702e96978a52a4427f6
csBlackWolf <30509936+csBlackWolf@users.noreply.github.com>
2022-06-15 03:28:43 +0200

    Add files via upload

---
+++ web/index.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html>
+  <head>
+  	<?php include ('head.php'); ?>
+    <link rel="stylesheet" href="style.css">
+  </head>
+  <body>
+    <?php include ('logo.php'); ?>
+    <?php include ('navi.php'); ?>
+  	<?php include ('body.php'); ?>
+  </body>
+</html>
