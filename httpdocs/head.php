27fbf3f3090c01fa5c9d37925f9ad32f0202503d
csBlackWolf <30509936+csBlackWolf@users.noreply.github.com>
2022-06-15 03:31:48 +0200

    Add files via upload

---
+++ httpdocs/head.php
@@ -0,0 +1,6 @@
+<meta charset="UTF-8">
+    <meta name="description" content="Der Gronkh Navigator">
+    <meta name="keywords" content="gronkh, navi, navigator, info, informationen, news, social, media, links, übersicht, stream, video, chat, shop">
+    <meta name="author" content="ZenBlackWolf">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Gronkh.net - Der Gronkh Navigator</title>
