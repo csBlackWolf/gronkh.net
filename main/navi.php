66e7f8ae75b698adfb6a3183d90e9d8d3f72f4d5
csBlackWolf <30509936+csBlackWolf@users.noreply.github.com>
2022-06-15 02:14:01 +0200

    Add files via upload

---
+++ main/navi.php
@@ -0,0 +1,52 @@
+<div class="fixed">
+	<button class="edit"><a href="#">Webseite Bearbeiten</a></button>
+</div>
+<nav>
+	<ul>
+		<li>
+			<a href="#">YouTube</a>
+		</li>
+		<li tabindex="0">
+			<a href="#">Stream</a>
+				<ul class="submenu">
+					<li>
+						<a tabindex="0" href="#2a">Live</a>
+					</li>
+					<li>
+						<a tabindex="0" href="#2b">24/7 TV</a>
+					</li>
+					<li>
+						<a tabindex="0" href="#2b">Archiv</a>
+					</li>
+				</ul>
+		</li>
+		<li tabindex="0">
+			<a href="#">Social Media</a>
+				<ul class="submenu">
+					<li>
+						<a href="#4a">Twitter</a>
+					</li>
+					<li>
+						<a href="#4b">Instergram</a>
+					</li>
+					<li>
+						<a href="#4c">Facebook</a>
+					</li>
+				</ul>
+		</li>
+		<li>
+			<a href="#">Info</a>
+				<ul class="submenu">
+					<li>
+						<a href="#4a">Wikipedia</a>
+					</li>
+					<li>
+						<a href="#4b">Gronkh Wiki</a>
+					</li>
+					<li>
+						<a href="#4c">Spieleliste</a>
+					</li>
+				</ul>
+		</li>
+	</ul>
+</nav>
